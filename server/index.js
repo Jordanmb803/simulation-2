789a3b0527aa62993ea0114b0656990ccd72e056
Jordan Buonforte <jordan@Jordans-MacBook-Air.local>
2018-05-11 16:56:32 -0600

    express.static working

--- server/index.js
+++ server/index.js
@@ -9,9 +9,13 @@ const port = 3555
 
 app.use(bodyParser.json())
 
+app.use(express.static(`./build`))
+
 massive(process.env.CONNECTION_STRING).then(dbInstance => {
     app.set('db', dbInstance)
 })
+
+
 
 // ENDPOINTS
 app.get('/api/properties', controller.getAllProps)
